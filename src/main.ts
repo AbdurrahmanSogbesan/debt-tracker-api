5611ef047404ff0d1e03d37ef4ea03bdb46dcb71
Abdulhaleem-6 <Abd6prif@gmail.com>
2024-11-04 16:01:42 +0100

    chore: port setting

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
-  await app.listen(3000);
+  await app.listen(process.env.port || 3000);
 }
 bootstrap();
