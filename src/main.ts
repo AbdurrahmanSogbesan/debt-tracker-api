ca2c5f484df04c8bf64d5736ced309af01eec6c6
Abdulhaleem-6 <Abd6prif@gmail.com>
2024-11-04 21:48:43 +0100

    error with the port

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  const port = process.env.port || 3000;
+  const port = process.env.PORT || 3000;
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   await app.listen(port, '0.0.0.0');
