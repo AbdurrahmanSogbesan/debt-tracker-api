d9c73dbdf5fbc647d65114cd8688a7faef9b0f32
Abdulhaleem-6 <Abd6prif@gmail.com>
2025-01-11 23:45:19 +0100

    feat: search transactions

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -14,6 +14,7 @@ import { JwtGuard } from '../auth/guard';
 import { GroupService } from '../group/group.service';
 import { GetTransactionsDto } from './dto/get-transactions.dto';
 import { Transaction } from '@prisma/client';
+import { SearchTransactionsDto } from './dto/search-transactions.dto';
 
 @UseGuards(JwtGuard)
 @Controller('transaction')
@@ -29,12 +30,29 @@ export class TransactionController {
     @Request() req,
   ): Promise<TransactionSummary> {
     const { id: supabaseUid } = req.user || {};
-
     const userId =
       await this.groupService.getUserIdFromSupabaseUid(supabaseUid);
 
     const transactionsQuery = { ...query, userId };
 
     return this.transactionService.getTransactions(transactionsQuery);
+  }
+
+  @Get('/search')
+  async searchTransactions(
+    @Query() query: SearchTransactionsDto,
+    @Request() req,
+  ) {
+    const { id: supabaseUid } = req.user || {};
+    const userId =
+      await this.groupService.getUserIdFromSupabaseUid(supabaseUid);
+
+    // Merge the validated query params with the userId
+    const searchParams = {
+      ...query,
+      userId,
+    };
+
+    return this.transactionService.searchTransactions(searchParams);
   }
 }
